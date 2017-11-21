43f6ab528387232fa9f482c1fc6e0d2e2ec39e8b
seseri <00ing.n@gmail.com>
2017-11-21 23:45:03 +0900

    Created cont_schedule job

---
+++ cont_web/app/jobs/cont_schedule_job.rb
@@ -0,0 +1,9 @@
+class ContScheduleJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    
+    #main loop of contest
+
+  end
+end
---
+++ cont_web/test/jobs/cont_schedule_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ContScheduleJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
