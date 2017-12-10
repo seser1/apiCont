54ffb994841be5012648d92bec470acd4dacd28f
seseri <00ing.n@gmail.com>
2017-12-10 17:45:12 +0900

    Crreated view of contests

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -1,14 +1,19 @@
 class WebController < ApplicationController
+  layout 'web_main'
+
   def main
     render 'web/top'
   end
 
   def list
+    @contests = Contest.all
     render 'web/list'
   end
 
   def cont
-    @contests = Contest.all
+    para=params[:id]
+    @contest = Contest.where(cont_id: para).first
+#    @contest = Contest.find(:first, :conditions => {:cont_id => para})
     render 'web/cont'
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   resources :users
+
+  root to: 'web#top'
+
+  get 'cont/:id', to: 'web#cont'
+
   get 'top', to: 'web#top'
   get 'list', to: 'web#list'
-  get 'cont', to: 'web#cont'
+#  get 'cont', to: 'web#cont'
+  get 'cont/:id', to: 'web#cont'
   get 'user', to: 'web#user'
 end
