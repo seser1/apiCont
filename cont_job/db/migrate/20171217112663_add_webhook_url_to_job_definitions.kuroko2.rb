b76f1050ceedf16d17e8b1b0356b1d43ccee552d
seseri <00ing.n@gmail.com>
2018-01-15 23:28:21 +0900

    Deleted cont_job (user active job to execute contests)

--- cont_job/db/migrate/20171217112663_add_webhook_url_to_job_definitions.kuroko2.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from kuroko2 (originally 26)
-class AddWebhookUrlToJobDefinitions < ActiveRecord::Migration[5.0]
-  def change
-    add_column :job_definitions, :webhook_url, :text
-  end
-end
