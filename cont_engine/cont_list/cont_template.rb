3bb4fca471fe6b44df27315ac33e97c6679a2beb
seser1 <00ing.n@gmail.com>
2018-01-23 00:02:43 +0900

    Updated .gitignore and cont_template

--- cont_engine/cont_list/cont_template.rb
+++ cont_engine/cont_list/cont_template.rb
@@ -3,24 +3,38 @@ require 'json'
 class ContestTemplate
   attr_accessor :run_flag
 
+  #Initializer may be overridden in inheriting class
+  #But then, variable defined here should be defined in inheriting class
   def initialize(users)
+    #Hash which describes data format send to user
     @data_template={}
+    #Hash which describes input json format from cont_web (also from user)
     @input_template={}
 
+    #Hash which contains data used in contest by user
     @data={}
+
+    #Hash which contains users information
     @users=users
+    #Html text which displays contest status (send to cont_web)
     @view=nil
+    #Input from user in current turn
     @input=nil
 
+    #flag which decide the contest's continuation
     @run_flag=true
   end
   
   def logic
-    #Write logic here
+    #Write contest's logic here
   end
 
+  def update_view
+    #Write code which generates latest @view
+  end
+  
   def update_stat
-    #Write code updates @run_flag
+  #Write code which updates @run_flag
   end
 
   #--Methods below aren't considered to be updated at inheriting class--
@@ -32,6 +46,8 @@ class ContestTemplate
     #Contest's main logic
     logic
 
+    #Update @view
+    update_view
     #Update @run_flag
     update_stat
   end
